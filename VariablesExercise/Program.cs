6bea922af8c10399b32a73308fe6d3f68976bac8
Andy Villalobos <Andy.villa012@gmail.com>
2023-01-27 11:07:51 -0800

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//declaring and initialize values
+string name = "Andy";
+int age = 30;
+char middleInitial = 'I';
+bool isOver18 = true;
+double currentTemp = 77.6;
+decimal currentPrice = 29.99m;
+
+//writing the variables to the console
+Console.WriteLine($"Name: {name}\n" +
+                  $"Age: {age}\n" +
+                  $"Middle Initial: {middleInitial}\n" +
+                  $"Is Over 18: {isOver18}\n" +
+                  $"Current Temp: {currentTemp}\n" +
+                  $"Current Price: {currentPrice}");
